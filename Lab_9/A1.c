b518a6bd7f9cef21427ba0712b0073af10430de7
Heet-Zalavadiya <heetzalavadia@gmail.com>
2026-02-14 19:11:31 +0530

    lab 9 notes

--- Lab_9/A1.c
+++ Lab_9/A1.c
@@ -2,6 +2,9 @@
 void main()
 {
     int x = 10;
-    printf("Value = %d, Address = %p\n", x,&x);
-}
+    // printf("Value = %d, Address = %p\n", x,&x);
 
+    int *ptr;
+    ptr = &x;
+    printf("Value = %d, Address = %p\n", *ptr, ptr);
+}
