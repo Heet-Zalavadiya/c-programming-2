931b52a019c0690b4e48d43ea258f6fcf08f0def
Heet-Zalavadiya <heetzalavadia@gmail.com>
2026-02-25 11:14:44 +0530

    Refactor code structure for improved readability and maintainability

--- Lab_11/A3.c
+++ Lab_11/A3.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 
-void main() {
+void main()
+{
 	char str[100];
-	char* p;
+	char *p;
 	p = str;
 	int len = 0;
-	
+
 	printf("Enter string: ");
 	gets(str);
-	
-	while(*p != '\0') {
+
+	while (*p != '\0')
+	{
 		len++;
 		p++;
 	}
-	
-	printf("Length of string is %d ",len-1);
+
+	printf("Length of string is %d ", len);
 }
