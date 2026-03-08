704de4c5eb0f13d47c55a3ff92fcc90292e23d72
Heet-Zalavadiya <heetzalavadia@gmail.com>
2026-03-08 11:17:21 +0530

    lab 12

---
+++ Lab_12/A1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main() {
+    int *ip;
+    char *cp;
+    float *fp;
+
+    ip = (int*) malloc(sizeof(int));
+    cp = (char*) malloc(sizeof(char));
+    fp = (float*) malloc(sizeof(float));
+
+    *ip = 10;
+    *cp = 'A';
+    *fp = 3.14;
+
+    printf("Integer value: %d\n", *ip);
+    printf("Character value: %c\n", *cp);
+    printf("Float value: %.2f\n", *fp);
+
+    free(ip);
+    free(cp);
+    free(fp);
+}
---
+++ Lab_12/A2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main() {
+    int n, i, sum = 0;
+    int *ptr;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    ptr = (int*) malloc(n * sizeof(int));
+
+    if(ptr == NULL) {
+        printf("Memory not allocated\n");
+        return;
+    }
+
+    printf("Enter %d numbers:\n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &ptr[i]); // (ptr + i)
+        sum += ptr[i];
+    }
+
+    printf("Sum = %d\n", sum);
+
+    free(ptr);
+}
